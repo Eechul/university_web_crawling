5631292d78527a060d0365ed60f488c1fa0c8d24
edongchel <choise154@gmail.com>
2016-07-07 21:38:18 +0900

    node-schedule 이용해 크롤링한 데이터 업데이트

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var cheerio = require('cheerio');
 var request = require('request');
+var cron = require('node-schedule');
 var app = express();
 
 var haksaUrl = "http://www.hs.ac.kr/kor/community/haksa_list.php"
@@ -20,6 +21,66 @@ conn.connect();
 
 var hacksaPosts = [];
 var scholarship = [];
+
+var rule = new cron.RecurrenceRule();
+rule.second = 30;
+cron.scheduleJob(rule, function(){
+    console.log(new Date(), '매 분 30초에 콜백');
+    request(haksaUrl, function (err, res, html) {
+      if (!err) {
+        var $ = cheerio.load(html);
+        $("td > a").each(function (i) {
+         var data = $(this)
+         //post.number=data.prev().html();
+         var title = data.text();
+         var link = data.attr("href");
+         var post = [" ", title, "-1", link, " "]
+         hacksaPosts.push(post);
+         return false;
+        })
+        $("tr").each(function () {
+          $(this).children('td:first-child').each(function(){
+            var data = $(this)
+            var number = data.text()
+            if(number){
+              hacksaPosts[i][0]=number;
+              i++;
+            }
+            return false;
+          })
+          $(this).children('td:nth-child(4)').each(function(){
+            var data = $(this)
+            var registration_date = data.text()
+            hacksaPosts[j][4] = registration_date;
+            j++;
+            return false;
+          })
+          return false;
+        })
+
+      }
+      var sql = "SELECT MAX(number) as number FROM board_hacksa"
+      conn.query(sql, function(err, results) {
+        if(err){
+          console.log(err);
+          res.status(500).send('SELECT MAX(number) DB server error!');
+        } else {
+          if(hacksaPosts[0][0] != results[0].number) {
+            var sql = "INSERT INTO board_hacksa SET ?"
+            conn.query(sql, [hacksaPosts], function(err, data) {
+              if(err){
+                console.log(err);
+                res.status(500).send('INSERT DB server error!');
+              } else {
+                console.log(new Date(), "hacksaPost Update!!");
+              }
+            })
+          }
+        }
+      })
+
+    });
+});
 
 app.get('/go', function(req, res) {
   var sql = "SELECT * FROM board_hacksa ORDER BY number DESC"
@@ -42,14 +103,13 @@ request(haksaUrl, function (err, res, html) {
       $("td > a").each(function (i) {
        var data = $(this)
        //post.number=data.prev().html();
-       var title=data.text();
-       var link=data.attr("href");
+       var title = data.text();
+       var link = data.attr("href");
        var post = [" ", title, "-1", link, " "]
        hacksaPosts.push(post);
       })
       var i = 0;
       var j = 0;
-      var flag = 0;
       $("tr").each(function () {
         $(this).children('td:first-child').each(function(){
           var data = $(this)
